029e7eb5ec62bf6856cf0aa1fadf252ecfdb6246
Matheus <theo.guimaraes@hotmail.com>
2025-04-19 12:26:59 -0300

    feat(playlist): add loading skeleton for user playlists in PlaylistCarousel component

--- src/components/playlist/playlistCarousel.tsx
+++ src/components/playlist/playlistCarousel.tsx
@@ -6,6 +6,8 @@ import Image from 'next/image'
 import { useEffect, useState } from 'react'
 import { useSession } from 'next-auth/react'
 import { useSpotifyApi } from '@/app/api/apiSpotify'
+import SkeletonPlaylistImage from '../skeleton/playlistImage'
+import { Skeleton } from '../ui/skeleton'
 
 interface UserPlaylistInterface {
   id: string
@@ -16,6 +18,7 @@ interface UserPlaylistInterface {
 export default function PlaylistCarousel() {
   const { data: session, status } = useSession()
   const api = useSpotifyApi()
+  const [isLoading, setIsLoading] = useState(true)
   const [userPlaylists, setUserPlaylists] = useState<UserPlaylistInterface[]>(
     [],
   )
@@ -28,9 +31,7 @@ export default function PlaylistCarousel() {
       const userId = session?.token.sub
 
       try {
-        const { data } = await api.get(
-          `/users/${userId}/playlists`,
-        )
+        const { data } = await api.get(`/users/${userId}/playlists`)
 
         const playlists = data.items.map(
           (item: { id: string; images: { url: string }[]; name: string }) => ({
@@ -41,6 +42,7 @@ export default function PlaylistCarousel() {
         )
 
         setUserPlaylists(playlists)
+        setIsLoading(false)
       } catch (error) {
         console.error('Erro ao buscar playlists:', error)
       }
@@ -52,25 +54,40 @@ export default function PlaylistCarousel() {
   return (
     <Carousel>
       <CarouselContent>
+        {isLoading &&
+          Array.from({ length: 5 }).map((_, i) => (
+            <CarouselItem
+              key={i}
+              className="flex-shrink-0 w-[132px] md:basis-1/2 lg:basis-1/3 xl:basis-1/4 2xl:basis-1/5 scale-95"
+            >
+              <SkeletonPlaylistImage />
+              <Skeleton className="overflow-hidden w-[132px] h-[36px] rounded-md mt-2" />
+            </CarouselItem>
+          ))}
+
         {userPlaylists.map((playlist) => (
           <CarouselItem
             key={playlist.id}
             className="flex-shrink-0 w-[132px] md:basis-1/2 lg:basis-1/3 xl:basis-1/4 2xl:basis-1/5 cursor-pointer scale-95 hover:scale-100 transform transition-transform duration-300"
           >
-            <Card className="h-[132px] w-[132px] p-0 rounded-md">
-              <CardContent className="p-0">
-                <Image
-                  src={playlist.image}
-                  alt="Playlist image"
-                  width={640}
-                  height={640}
-                  className="rounded-md"
-                />
-              </CardContent>
-            </Card>
-            <p className="text-sm mt-1 tracking-wider line-clamp-2 overflow-hidden w-[132px]">
-              {playlist.name}
-            </p>
+            {!isLoading && (
+              <>
+                <Card className="h-[132px] w-[132px] p-0 rounded-md">
+                  <CardContent className="p-0">
+                    <Image
+                      src={playlist.image}
+                      alt="Playlist image"
+                      width={640}
+                      height={640}
+                      className="rounded-md"
+                    />
+                  </CardContent>
+                </Card>
+                <p className="text-sm mt-1 tracking-wider line-clamp-2 overflow-hidden w-[132px]">
+                  {playlist.name}
+                </p>
+              </>
+            )}
           </CarouselItem>
         ))}
       </CarouselContent>
---
+++ src/components/skeleton/playlistImage.tsx
@@ -0,0 +1,8 @@
+import { Skeleton } from "../ui/skeleton";
+
+export default function SkeletonPlaylistImage(){
+  return(
+    <Skeleton className="h-[132px] w-[132px] p-0 rounded-md">
+    </Skeleton>
+  )
+}
