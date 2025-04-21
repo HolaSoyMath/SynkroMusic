68a762a8e83d1f95aa8f671e5042d8b602fe08e2
Matheus <theo.guimaraes@hotmail.com>
2025-04-21 17:51:48 -0300

    refactor: remove debug logging from sumMsDurationPlaylist function

--- src/utils/sumMsDurationPlaylist.ts
+++ src/utils/sumMsDurationPlaylist.ts
@@ -1,8 +1,6 @@
 import { SpotifyTrack } from '@/interface/SpotifyTrack'
 
 export function sumMsDurationPlaylist(tracks: SpotifyTrack[]) {
-  console.log("tracks:", tracks);
-  
   return tracks.reduce((sum, track) => {
     return sum + track.duration_ms
   }, 0)
