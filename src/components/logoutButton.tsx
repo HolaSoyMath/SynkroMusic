3bd7571e8ff7daa724f928ef41ccfc3b803de8d8
Matheus <theo.guimaraes@hotmail.com>
2025-05-03 14:53:02 -0300

    update: Att logout function

--- src/components/logoutButton.tsx
+++ src/components/logoutButton.tsx
@@ -1,19 +1,20 @@
 'use client'
 
-import { signOut } from "next-auth/react"
-import { Button } from "./ui/button"
-import { LogOut } from "lucide-react"
+import { Button } from './ui/button'
+import { LogOut } from 'lucide-react'
 
 export default function LogoutButton() {
+  function signOut() {
+    localStorage.clear()
+    window.location.href = '/'
+  }
   return (
     <Button
       className="w-full rounded-4 bg-transparent text-background border-1 border-background cursor-pointer px-0 py-5 relative hover:bg-primary"
       onClick={() => signOut()}
     >
       <LogOut />
-      <span className="uppercase text-md tracking-widest">
-        Sair
-      </span>
+      <span className="uppercase text-md tracking-widest">Sair</span>
     </Button>
   )
 }
