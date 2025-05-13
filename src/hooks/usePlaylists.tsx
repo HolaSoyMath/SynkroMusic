c0f486f3f2a664d29ea49a2b44f1a7779247ae15
Matheus Santos <msantos@gocn.com.br>
2025-05-13 00:28:12 -0300

    refactor(usePlaylists): simplify userId extraction from API response
    The redundant extraction of userId from the API response was simplified by directly accessing the data.id property, improving code readability and maintainability.

--- src/hooks/usePlaylists.tsx
+++ src/hooks/usePlaylists.tsx
@@ -16,12 +16,7 @@ export function usePlaylists(token: string | undefined) {
 
     async function load() {
       try {
-<<<<<<< HEAD
         const userId = await (await api.get('/me')).data.id
-=======
-        const respSpotify = (await api.get('/me')).data
-        const userId = respSpotify.id
->>>>>>> c7437174e9bfe8db20f2471f6b9e1e65e400398d
 
         const { data } = await api.get(`/users/${userId}/playlists`)
         const playlists = await data.items.map(
